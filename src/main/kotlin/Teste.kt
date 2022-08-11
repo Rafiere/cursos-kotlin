c74fa484c652a15d2af39fc34c026eebe68e386c
Rafael <rafapgbr@gmail.com>
2022-08-10 21:31:12 -0300

    feat: adição do teste da aula 02.

--- src/main/kotlin/Teste.kt
+++ src/main/kotlin/Teste.kt
@@ -3,7 +3,7 @@ fun main(){
     //"var": O valor de uma variável pode ser alterado.
     var titular: String = "Alex" //O Kotlin realiza a inferência de tipos quando atribuímos o valor a uma variável.
     val numeroConta: Int = 1000
-    var saldo: Double = 0.0
+    var saldo: Double = 2.0
 
     print("Nome do titular: $titular") //O "$" realiza a concatenação.
 
@@ -11,4 +11,44 @@ fun main(){
     //estaticamente tipado, ele apenas não força que o tipo da variável seja declarado.
 
     //Todas as variáveis do Kotlin devem ser inicializadas.
+
+//    if(saldo > 0.0){
+//        println("\nA conta é positiva!")
+//    } else if(saldo == 0.0){
+//        println("\nA conta é neutra!")
+//    } else {
+//        println("A conta é negativa")
+//    }
+
+    //O "when" abaixo se refere ao "if()" acima.
+    testaCondicoes(saldo)
+
+    //Quando cada condição do "when" possui essa estrutura, podemos retirar as chaves.
+    when {
+        saldo > 0.0 -> println("\nA conta é positiva!")
+        saldo == 0.0 -> println("\nA conta é neutra!")
+        else -> println("A conta é negativa")
+    }
+
+    for(i in 1..3){
+        print("$i: Testando\n")
+    }
+
+    for(i in 5 downTo 1 step 2){
+        print("$i\n")
+    }
+}
+
+private fun testaCondicoes(saldo: Double) {
+    when {
+        saldo > 0.0 -> {
+            println("\nA conta é positiva!")
+        }
+        saldo == 0.0 -> {
+            println("\nA conta é neutra!")
+        }
+        else -> {
+            println("A conta é negativa")
+        }
+    }
 }
